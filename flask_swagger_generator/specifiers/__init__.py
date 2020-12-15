99baa7d39dea1dd72795e4f1eb9e8b208631b9da
DUYN <marcvanduyn1@gmail.com>
2020-12-15 23:33:33 +0100

    feat(SWAGGER_GENERATOR): add generator export
    add generator export

---
+++ flask_swagger_generator/specifiers/__init__.py
@@ -0,0 +1,7 @@
+from flask_swagger_generator.specifiers.swagger_version import SwaggerVersion
+from flask_swagger_generator.specifiers.swagger_three_specifier import \
+    SwaggerThreeSpecifier
+from flask_swagger_generator.specifiers.swagger_request_types import \
+    SwaggerRequestType
+
+__all__ = ['SwaggerVersion', 'SwaggerThreeSpecifier', 'SwaggerRequestType']
