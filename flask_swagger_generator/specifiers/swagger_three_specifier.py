8fd6f7930e8fa566c132d71153b00d127028cf6b
fiza.ashraf <fiza.ashraf@afiniti.com>
2022-10-14 18:23:24 +0500

    Issue#28 : New Feature - Support for nested schema for json request/response

--- flask_swagger_generator/specifiers/swagger_three_specifier.py
+++ flask_swagger_generator/specifiers/swagger_three_specifier.py
@@ -68,7 +68,7 @@ class SwaggerSchema(SwaggerModel):
             for key in schema:
                 self.properties[key] = "type: {}".format(
                     self.get_type(schema[key]).value
-                )
+                ) + "\n\t\t      example: {}".format(schema[key])
 
         elif isinstance(schema, MarshmallowSchema):
 
@@ -118,6 +118,8 @@ class SwaggerSchema(SwaggerModel):
             return InputType.NUMBER
         elif isinstance(value, list):
             return InputType.ARRAY
+        elif isinstance(value, dict):
+            return InputType.OBJECT
         else:
             SwaggerGeneratorException(
                 "Type {} is not supported".format(type(value))
